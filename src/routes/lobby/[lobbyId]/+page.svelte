5b5134ee268da73eba7cc74510f4580a990cff34
faredun <git@faredun.com>
2024-09-28 00:48:11 +0530

    added: mobile responsiveness

--- src/routes/lobby/[lobbyId]/+page.svelte
+++ src/routes/lobby/[lobbyId]/+page.svelte
@@ -10,9 +10,17 @@
 
 <p class="text-center text-lg mb-4">lobby id: {lobbyId}</p>
 
-<div class="grid grid-rows-4 grid-flow-col gap-2 h-full min-h-[78vh]">
-    <div class="border row-span-3 col-span-2 flex justify-center items-center">game</div>
-    <div class="border col-span-2 flex justify-center items-center">game status</div>
-    <div class="border flex justify-center items-center">log</div>
-    <div class="border row-span-3 flex justify-center items-center">chat</div>
+<div
+    class="grid grid-rows-6 grid-flow-row md:grid-rows-4 md:grid-flow-col gap-2 h-full min-h-[78vh]"
+>
+    <div
+        class="border row-span-3 col-span-2 md:row-span-3 md:col-span-3 flex justify-center items-center"
+    >
+        game
+    </div>
+    <div class="border md:col-span-3 flex justify-center items-center">game status</div>
+    <div class="border flex justify-center items-center md:col-span-2">log</div>
+    <div class="border row-span-2 col-span-2 md:row-span-3 flex justify-center items-center">
+        chat
+    </div>
 </div>
