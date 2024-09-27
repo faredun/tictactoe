a6a347426328004b4eddc913c332c7b9292b84eb
faredun <git@faredun.com>
2024-09-27 18:43:21 +0530

    setup: added shadcn - dark/light mode, tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import Button from '$lib/components/ui/button/button.svelte';
+    import ToggleTheme from '../ui/ToggleTheme.svelte';
+</script>
+
+<ToggleTheme />
+<h1 class="text-2xl">hello</h1>
+<Button>click me</Button>
